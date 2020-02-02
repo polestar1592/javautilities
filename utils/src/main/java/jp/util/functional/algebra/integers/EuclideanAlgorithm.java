e7e0d91a4bf2f57203f10e732db4a684c8fead00
Keitaro Kitagawa <polestar1592@yahoo.co.jp>
2020-02-02 21:24:00 +0900

    fix: 拡張ユークリッド互除法の戻り値が left以外バグっていたため修正

--- utils/src/main/java/jp/util/functional/algebra/integers/EuclideanAlgorithm.java
+++ utils/src/main/java/jp/util/functional/algebra/integers/EuclideanAlgorithm.java
@@ -9,31 +9,42 @@ public final class EuclideanAlgorithm {
     }
 
     public static long calculateGcd(long a, long b) {
-        return solveEquation(a, b).getRight();
+        return solve(a, b).getRight();
     }
 
-    public static long calculateInverse(long x, long p) {
-        return solveEquation(x, p).getLeft();
+    public static long calculateInverse(long x, long prime) {
+        return solve(x, prime).getLeft();
     }
 
     // solve ax + by = gcd(a, b).
     // returns (x, y, gcd(a, b))
-    private static Triple<Long, Long, Long> solveEquation(long a, long b) {
-        long s = a;
-        long t = b;
-        long u = 1;
-        long v = 0;
-        long w = 0;
+    public static Triple<Long, Long, Long> solve(long a, long b) {
+        // TODO 戻り値 tripleはちょっとキモイ。 それ用の型を作っても良い。
+        int signA = a < 0 ? -1 : 1;
+        int signB = b < 0 ? -1 : 1;
 
+        long s = signA * a;
+        long t = signB * b;
+        long v0 = 0, v1 = 1;
+        long u0 = 1, u1 = 0;
         while (s > 0) {
             long q = t / s;
-            w = t - q * s;
+            // calculate gcd
+            long w = t % s;
             t = s;
             s = w;
-            w = v - q * u;
-            v = u;
-            u = w;
+
+            // calculate v0, u0, v1, u1
+            // s.t. v0*a + u0*b = t && v1*a + u1*b = s
+            w = v0 - q * v1;
+            v0 = v1;
+            v1 = w;
+
+            w = u0 - q * u1;
+            u0 = u1;
+            u1 = w;
+
         }
-        return Triple.of(v, u, Math.abs(w));
+        return Triple.of(signA * v0, signB * u0, t);
     }
 }
