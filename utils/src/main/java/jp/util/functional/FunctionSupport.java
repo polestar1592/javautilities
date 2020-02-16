7273ca320fb7f049733df2ef014cd89df038a120
Shota Arai <shota.basketball.13@gmail.com>
2020-02-16 20:36:55 +0900

    変数名修正

--- utils/src/main/java/jp/util/functional/FunctionSupport.java
+++ utils/src/main/java/jp/util/functional/FunctionSupport.java
@@ -8,23 +8,23 @@ public class FunctionSupport {
     private FunctionSupport() {
     }
 
-    public static <T, U, R> Function<U, R> partial(BiFunction<T, U, R> biFunction, T t) {
-        return u -> biFunction.apply(t, u);
+    public static <T, U, R> Function<U, R> partial(BiFunction<T, U, R> f, T t) {
+        return u -> f.apply(t, u);
     }
 
-    public static <S, T, U, R> BiFunction<T, U, R> partial(TriFunction<S, T, U, R> triFunction, S s) {
-        return (t, u) -> triFunction.apply(s, t, u);
+    public static <S, T, U, R> BiFunction<T, U, R> partial(TriFunction<S, T, U, R> f, S s) {
+        return (t, u) -> f.apply(s, t, u);
     }
 
-    public static <S, T, U, R> Function<U, R> partial(TriFunction<S, T, U, R> triFunction, S s, T t) {
-        return u -> triFunction.apply(s, t, u);
+    public static <S, T, U, R> Function<U, R> partial(TriFunction<S, T, U, R> f, S s, T t) {
+        return u -> f.apply(s, t, u);
     }
 
-    public static <T, U, R> Function<T, Function<U, R>> curried(BiFunction<T, U, R> biFunction) {
-        return t -> (u -> biFunction.apply(t, u));
+    public static <T, U, R> Function<T, Function<U, R>> curried(BiFunction<T, U, R> f) {
+        return t -> (u -> f.apply(t, u));
     }
 
-    public static <S, T, U, R> Function<S, Function<T, Function<U, R>>> curried(TriFunction<S, T, U, R> biFunction) {
-        return s -> (t -> (u -> biFunction.apply(s, t, u)));
+    public static <S, T, U, R> Function<S, Function<T, Function<U, R>>> curried(TriFunction<S, T, U, R> f) {
+        return s -> (t -> (u -> f.apply(s, t, u)));
     }
 }
