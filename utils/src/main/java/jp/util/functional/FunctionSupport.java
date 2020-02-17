2015a6ebe715a2d203427e79af699eced751ad6f
Shota Arai <shota.basketball.13@gmail.com>
2020-02-17 21:51:27 +0900

    型境界を指定

--- utils/src/main/java/jp/util/functional/FunctionSupport.java
+++ utils/src/main/java/jp/util/functional/FunctionSupport.java
@@ -8,31 +8,31 @@ public class FunctionSupport {
     private FunctionSupport() {
     }
 
-    public static <T, U, R> Function<U, R> partial(BiFunction<T, U, R> f, T t) {
+    public static <T, U, R> Function<U, R> partial(BiFunction<? super T, ? super U, ? extends R> f, T t) {
         return u -> f.apply(t, u);
     }
 
-    public static <S, T, U, R> BiFunction<T, U, R> partial(TriFunction<S, T, U, R> f, S s) {
+    public static <S, T, U, R> BiFunction<T, U, R> partial(TriFunction<? super S, ? super T, ? super U, ? extends R> f, S s) {
         return (t, u) -> f.apply(s, t, u);
     }
 
-    public static <S, T, U, R> Function<U, R> partial(TriFunction<S, T, U, R> f, S s, T t) {
+    public static <S, T, U, R> Function<U, R> partial(TriFunction<? super S, ? super T, ? super U, ? extends R> f, S s, T t) {
         return u -> f.apply(s, t, u);
     }
 
-    public static <T, U, R> Function<T, Function<U, R>> curried(BiFunction<T, U, R> f) {
+    public static <T, U, R> Function<T, Function<U, R>> curried(BiFunction<? super T, ? super U, ? extends R> f) {
         return t -> (u -> f.apply(t, u));
     }
 
-    public static <S, T, U, R> Function<S, Function<T, Function<U, R>>> curried(TriFunction<S, T, U, R> f) {
+    public static <S, T, U, R> Function<S, Function<T, Function<U, R>>> curried(TriFunction<? super S, ? super T, ? super U, ? extends R> f) {
         return s -> (t -> (u -> f.apply(s, t, u)));
     }
 
-    public static <T, U, R> R apply(BiFunction<T, U, R> f, Pair<T, U> pair) {
+    public static <T, U, R> R apply(BiFunction<? super T, ? super U, ? extends R> f, Pair<T, U> pair) {
         return f.apply(pair.getLeft(), pair.getRight());
     }
 
-    public static <S, T, U, R> R apply(TriFunction<S, T, U, R> f, Triple<S, T, U> triple) {
+    public static <S, T, U, R> R apply(TriFunction<? super S, ? super T, ? super U, R> f, Triple<S, T, U> triple) {
         return f.apply(triple.getLeft(), triple.getMiddle(), triple.getRight());
     }
 }
